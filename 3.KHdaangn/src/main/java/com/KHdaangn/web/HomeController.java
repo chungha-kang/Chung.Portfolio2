71ad5cd62e08540c6c26654a6478a656316b70bf
chungha-kang <cubone89@gmail.com>
2022-11-17 16:33:02 +0900

    HomeController logger 출력 텍스트 오타 수정

--- 3.KHdaangn/src/main/java/com/KHdaangn/web/HomeController.java
+++ 3.KHdaangn/src/main/java/com/KHdaangn/web/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("inddex", locale);
+		logger.info("index", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
