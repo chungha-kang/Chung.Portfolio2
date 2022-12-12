f327a9924f1ac45f690fd44297a0aa6591a1ff34
chungha-kang <cubone89@gmail.com>
2022-12-12 11:44:12 +0900

    JobsController 오타 수정

--- 03.KHdaangn/src/main/java/com/KHdaangn/web/jobs/controller/JobsController.java
+++ 03.KHdaangn/src/main/java/com/KHdaangn/web/jobs/controller/JobsController.java
@@ -72,8 +72,7 @@ public class JobsController {
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		String formatte
-		dDate = dateFormat.format(date);
+		String formattedDate = dateFormat.format(date);
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "jobs/job-posts";
